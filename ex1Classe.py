789259dd1f42d35a043330306e61b5b30fb26bcd
JuanPa0000 <jfuentes20943@iesjoanramis.org>
2024-11-06 22:49:21 +0100

    Exercici classe 1

---
+++ ex1Classe.py
@@ -0,0 +1,29 @@
+a = "Hola"
+b = input("Llegir paraula: ")
+
+# Mostri caracters de la paraula llegida
+for i in b:
+    print(i)
+# Longitud paraula
+print(len(b))
+
+# Comparar-les
+if a == b:
+    print(f"{a} i {b} són iguals")
+else:
+    print(f"{a} i {b} no són iguals")
+
+# juntar-les amb un guió
+print(a+"-"+b)
+
+# Repetir-la 3 vegades
+print(b*3)
+
+# trobar una lletra dins b
+if "a" in b:
+    print(f"a és dins l'string {b}")
+else:
+    print("a no hi és")
+
+lb = [1, "hola", (3, 4), (1, 2)]
+print(lb[2])
